4409e7a54bd61eefa21012ecd7f6c5d7f91641ad
Hhpon <624791164@qq.com>
2019-05-12 20:37:08 +0800

    体验小程序云开发

---
+++ miniprogram/pages/blog/blog.js
@@ -0,0 +1,97 @@
+// miniprogram/pages/blog/blog.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    blogArr: []
+  },
+  
+  getUserinfo(e) {
+    console.log(e.detail.userInfo);
+    
+  },
+
+  getOpenid() {
+    wx.cloud.callFunction({
+      name: 'login'
+    }).then((result) => {
+      console.log(result.result.openid);
+      try {
+        wx.setStorageSync('openId', result.result.openid)
+      } catch (e) {
+
+      }
+    }).catch((err) => {
+
+    });
+  },
+
+  getBlog() {
+    const db = wx.cloud.database()
+    db.collection('counters').where({ content: '密集安排' }).get().then(res => {
+      console.log(res);
+      this.setData({
+        blogArr: res.data
+      })
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.getBlog()
+    this.getOpenid()
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
